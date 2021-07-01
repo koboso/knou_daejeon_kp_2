af3f9b0f24f3e777fd9697288674c8353084ea35
ONZ1 <onzi1230@gmail.com>
2021-07-01 23:24:13 +0900

    add Bee's movement
    벌 스폰 위치에 따라 중앙으로 날아감. plant와 bee에 충돌 처리 추가. 충돌 시 식물 색 변화.

--- PlantGame/Assets/Scripts/BeeSpawnManager.cs
+++ PlantGame/Assets/Scripts/BeeSpawnManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BeeSpawnManager : MonoBehaviour
 {
+
     public float spwanTime = 3f;
     public float curTime;
     public Transform[] spawnPoints;
@@ -13,15 +14,20 @@ public class BeeSpawnManager : MonoBehaviour
     {
         if(curTime >= spwanTime)
         {
+            //벌 스폰포인트 인덱스가 될 랜덤한 수 
             int x = Random.Range(0,spawnPoints.Length);
+            //랜덤 인덱스에 해당하는 스폰포인트에서 벌 생성
             SpawnBee(x);
         }
         curTime += Time.deltaTime;
     }
 
+    //벌 생성하는 함수
     public void SpawnBee(int ranNum)
     {
+        //스폰타이머 리셋
         curTime = 0;
+        //랜덤 인덱스에 해당하는 스폰포인트에서 벌 생성
         Instantiate(bee,spawnPoints[ranNum]);
     }
 }
