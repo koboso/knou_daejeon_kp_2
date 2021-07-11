1650cce086025b6d21e5dba75779563a99be673c
hjpark9999 <hjpark9999@knou.ac.kr>
2021-07-11 22:32:49 +0900

    Today(당일), AllTime(지금까지의 모든 날) 최고 점수 기록. 피버타임때 나무성장속도 증가.

--- PlantGame/Assets/Scripts/GameOverScreen.cs
+++ PlantGame/Assets/Scripts/GameOverScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 
 // 게임이 종료 되었을 때 사용하는 화면
@@ -11,22 +12,40 @@ public class GameOverScreen : MonoBehaviour
     public Text beesKilled;
     public Text highScoreText;
 
-    public float highScore ; // 최고점 -----
+
+    private float allTimeHighScore;
+    private string allTimeHighDate;
+    private float todayHighScore;
+    private string todayHighDate;
+    private string today;
 
     public void Setup(float score, int bee)
     {
         gameObject.SetActive(true);
 
-        if (score > highScore)
+        if (todayHighScore < score ) // 하루 최고점 기록
         {
-            highScore = score;
-            PlayerPrefs.SetFloat("HighScore", highScore);
-            highScoreText.text = "NEW HIGHSCORE !!";
+            todayHighScore = score;
+            todayHighDate = today;
+            PlayerPrefs.SetFloat("TodayHighScore", todayHighScore);
+            PlayerPrefs.SetString("TodayHighDate", todayHighDate);
+            highScoreText.text = "TODAY'S BEST !!";
+
+            if(allTimeHighScore < score)  // 지금까지의 최고점 기록
+            {
+                allTimeHighScore = score;
+                allTimeHighDate = today;
+                PlayerPrefs.SetFloat("AllTimeHighScore", allTimeHighScore);
+                PlayerPrefs.SetString("AllTimeHighDate", allTimeHighDate);
+                highScoreText.text = "ALLTIME BEST !!";
+
+            }
+            PlayerPrefs.Save();
         }
         else
         {
-            highScoreText.text = "High Score: " + 
-                string.Format("{0:N2}",PlayerPrefs.GetFloat("HighScore")) + " m";
+            highScoreText.text = "Today High: " + 
+                string.Format("{0:N2}",PlayerPrefs.GetFloat("TodayHighScore")) + " m";
         }
 
         treeHeight.text = "\nTree is grown " + string.Format("{0:N2}", score) + " m";
@@ -38,9 +57,22 @@ public class GameOverScreen : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    // 하루와 지금까지의 최고점수 
     void Awake()
     {
-        highScore = PlayerPrefs.GetFloat("HighScore");
+        DateTime dt = DateTime.Now;  // 
+        string today = dt.ToString("yyyy-MM-dd");
+        Debug.Log(today);
 
+        allTimeHighScore = PlayerPrefs.GetFloat("AllTimeHighScore");
+        allTimeHighDate = PlayerPrefs.GetString("AllTimeHighDate");
+        todayHighScore = PlayerPrefs.GetFloat("TodayHighScore");
+        todayHighDate = PlayerPrefs.GetString("TodayHighDate");
+
+        if (todayHighDate != today)
+        {
+            todayHighDate = today;
+            todayHighScore = 0;
+        }
     }
 }
--- PlantGame/Assets/Scripts/Logic.cs
+++ PlantGame/Assets/Scripts/Logic.cs
@@ -10,7 +10,7 @@ public class Logic : MonoBehaviour{
 
     public float growthSpeed = 0.003f;      // 프레임당 성장 속도 (프레임당)
     public float clickGrowthSpeed = 0.2f;   // 클릭시 성장 속도 (클릭당)
-    public float feverGrowthSpeed = 0.006f; // 피버 성장속도 (프레임당)
+    public float feverGrowthSpeed = 0.009f; // 피버 성장속도 (프레임당)
     public float treeHeight = 0f;
     public int beesKilled = 0;
     public int feverpoint = 0;
