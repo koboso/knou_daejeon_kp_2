95fff251663ed3bfd546f6dfb719445d3d8a78c8
ONZ1 <onzi1230@gmail.com>
2021-07-20 00:59:12 +0900

    게임오버시 오버화면 아닌 리스타트버튼 작동X

--- PlantGame/Assets/Scripts/ReStartGame.cs
+++ PlantGame/Assets/Scripts/ReStartGame.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ReStartGame : MonoBehaviour
 {
+    private Logic logic;
+    private Button thisbutton;
+    private void Start()
+    {
+        logic = GameObject.Find("/GameManager").GetComponent<Logic>();
+        thisbutton = GetComponent<Button>();
+    }
+
+    //게임오버화면이 아닌 리스타트버튼은 게임오버화면에서 작동 불가
+    private void Update()
+    {
+        if (gameObject.transform.parent.name != "GameOverScreen")
+        {
+            if (logic.state == Logic.GameState.GAMEOVER)
+            { thisbutton.interactable = false; }
+            else thisbutton.interactable = true;
+        }
+    }
 
     // 리스타트 버튼 클릭시 실행되는 코드.
     public void NewGame()
     {
         GameObject.Find("GameManager").GetComponent<Logic>().state = Logic.GameState.READY;
         //transform.parent.GetComponent<GameOverScreen>().Hide();
-        if(gameObject.transform.parent.name == "GameOverScreen")
+        if (gameObject.transform.parent.name == "GameOverScreen")
         { transform.parent.GetComponent<GameOverScreen>().Hide(); }
     }
 
